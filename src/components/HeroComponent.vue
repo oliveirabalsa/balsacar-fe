0d9e5fb159c6b942d7e8df91741d11ba6b3b51ac
Leonardo <oliveirabalsa2@gmail.com>
2024-02-27 19:16:56 -0300

    chore: add tailwind

---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+console.log(ref)
+</script>
+
+<template>
+  <div class="bg-black h-screen">
+    <img class="w-3/5" src="../assets/car.png" alt="" />
+  </div>
+</template>
