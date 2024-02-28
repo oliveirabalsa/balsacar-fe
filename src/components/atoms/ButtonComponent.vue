f536e94cb76206c44cf3c593fa5961ffd79c9d88
Leonardo <oliveirabalsa2@gmail.com>
2024-02-28 06:54:54 -0300

    feat(atom): create button component

---
+++ src/components/atoms/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({
+  type: {
+    type: String,
+    default: 'submit'
+  }
+})
+</script>
+<template>
+  <button :type="type" class="text-white border border-primary rounded-lg px-5 py-2">
+    <slot />
+  </button>
+</template>
