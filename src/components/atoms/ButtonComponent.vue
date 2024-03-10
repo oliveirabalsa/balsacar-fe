e0da99959ae42307661ad9c2c496debb90717223
Leonardo <oliveirabalsa2@gmail.com>
2024-03-10 14:51:49 -0300

    fix: add default values properly

--- src/components/atoms/ButtonComponent.vue
+++ src/components/atoms/ButtonComponent.vue
@@ -4,19 +4,16 @@ type ButtonProps = {
   variant?: 'transparent' | 'primary' | 'rounded'
 }
 
-const { type, variant } = defineProps<ButtonProps>()
+const { type = 'submit', variant = 'primary' } = defineProps<ButtonProps>()
 
-const classes: Record<string, string> = {
+const classes: Record<typeof variant, string> = {
   transparent: 'text-white border border-primary rounded-lg px-5 py-2 hover:bg-primary',
   primary: 'text-white rounded-lg px-5 py-2 bg-primary hover:opacity-90',
   rounded: 'text-white px-5 py-2 bg-primary hover:opacity-90 rounded-full'
 }
 </script>
 <template>
-  <button
-    :type="type || 'submit'"
-    :class="classes[(variant as ButtonProps['variant']) || 'primary']"
-  >
+  <button :type="type" :class="classes[variant] + 'cursor-pointer'">
     <slot />
   </button>
 </template>
