617504cdc98089f94882a4a688c1681ff5a16892
Leonardo Oliveira Balsalobre <leonardo@leonardo>
2024-04-20 23:44:42 -0300

    feat: add dta to page

--- src/views/CarsView.vue
+++ src/views/CarsView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import axios from '@/services/axios/axios'
 import SearchComponent from '@/components/molecules/SearchComponent.vue'
 
 const route = useRoute()
+const data = ref(null)
 
 onMounted(async () => {
   try {
@@ -17,6 +18,7 @@ onMounted(async () => {
       }
     })
     const response = await axios.get(url.toString())
+    data.value = response.data
 
     console.log(response.data)
   } catch (error) {
@@ -28,5 +30,6 @@ onMounted(async () => {
   <SearchComponent />
   <section>
     Cars vue with filters: {{ route.query }}
+    Result {{ data }}
   </section>
 </template>
